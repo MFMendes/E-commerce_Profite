daf596dd79c6f90033f6433b090509913737dc59
unknown <marcelofariam23@outlook.com>
2021-07-07 00:41:45 -0300

    Section Products

--- js/script.js
+++ js/script.js
@@ -39,3 +39,27 @@ var swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true,
 });
+
+var swiper = new Swiper('.swiper-container-products', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // init: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  }
+});
